31b667a8bb498f4e84cc2066586ef540ceb39516
Michael Dunnegan <mdunnegan@spicy.io>
2014-03-06 12:22:06 -0500

    installed rspec

---
+++ SND/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SND::Application.config.secret_key_base = 'cc7fd198d0f8eac88384b4168da038a7a6f4e529140f99725e0c59e1363fb70fb886edd334a0d1ad0a8e1273a891cdfd1dbb36652dccdc7a20699e98fd97de67'
